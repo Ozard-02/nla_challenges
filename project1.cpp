f819249cd7b9177935225d28e917164d872850ee
Ozard-02 <expoking2017@gmail.com>
2024-09-23 14:20:20 +0200

    inizializzazione del progetto

---
+++ project1.cpp
@@ -0,0 +1,10 @@
+#include <Eigen/Dense>
+#include <iostream>
+
+// from https://github.com/nothings/stb/tree/master
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+
+using namespace Eigen;
