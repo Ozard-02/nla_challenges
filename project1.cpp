55562a4c2d389913e18825c43779664de8fa1369
Ozard-02 <expoking2017@gmail.com>
2024-09-23 14:33:59 +0200

    secondo

--- project1.cpp
+++ project1.cpp
@@ -8,3 +8,23 @@
 #include "stb_image_write.h"
 
 using namespace Eigen;
+
+MatrixXd random_Noise_Generator(const MatrixXd& image) {
+  MatrixXd noise= MatrixXd::Random(image.rows(), image.cols());
+  MatrixXd noise_Added_Image = image + 50*noise;
+
+  return noise_Added_Image;
+}
+
+int main(int argc, char* argv[]){
+    if (argc < 2) {
+        std::cerr << "Usage: " << argv[0] << " <image_path>" << std::endl;
+    return 1;
+    }
+    const char* input_image_path = argv[1];
+    int width, height;
+    unsigned char* image_data = stbi_load(input_image_path, &width, &height);  // Force load as RGB
+
+
+    return 0;
+}
