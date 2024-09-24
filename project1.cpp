b09d40f32770faa20a884d40f8471540b1ea822a
giorgiovenezia <giorgio.venezia@mail.polimi.it>
2024-09-24 08:56:02 +0000

    task 3 and 4

--- project1.cpp
+++ project1.cpp
@@ -7,6 +7,7 @@
 #include "stb_image_write.h"
 
 using namespace Eigen;
+using namespace std;
 
 MatrixXd random_Noise_Generator(const MatrixXd& image) {
   MatrixXd noise= MatrixXd::Random(image.rows(), image.cols());
@@ -16,33 +17,46 @@ MatrixXd random_Noise_Generator(const MatrixXd& image) {
 }
 
 int main(int argc, char* argv[]){
-    if (argc < 2) {
-        std::cerr << "Usage: " << argv[0] << " <image_path>" << std::endl;
+  if (argc < 2) {
+    cerr << "Usage: " << argv[0] << " <image_path>" << endl;
     return 1;
-    }
-    const char* input_image_path = argv[1];
-    int width, height, channels;
-    unsigned char* image_data = stbi_load(input_image_path, &width, &height,&channels, 1);
-    MatrixXd image(height, width);
-    std::cout << "Image size: " << height << "x" << width << std::endl;
-    for (int i = 0; i < height; ++i) {
+  }
+  const char* input_image_path = argv[1];
+  // Task 1 //
+  int width, height, channels;
+  unsigned char* image_data = stbi_load(input_image_path, &width, &height,&channels, 1);
+  MatrixXd image(height, width);
+  cout << "Image size: " << height << "x" << width << endl;
+  for (int i = 0; i < height; ++i) {
     for (int j = 0; j < width; ++j) {
       int index = (i * width + j);
       image(i, j) = static_cast<double>(image_data[index] / 255.0);
-        }
     }
-    stbi_image_free(image_data);
-    std::cout<<"the matrix has:"<<image.rows()<<"rows and"<<image.cols()<<"columns"<<std::endl;
+  }
+  stbi_image_free(image_data);
+  cout<<"The matrix has: "<<image.rows()<<" rows and "<<image.cols()<<" columns"<<endl;
 
-    MatrixXd noisy_image = random_Noise_Generator(image);
-    Matrix<unsigned char, Dynamic, Dynamic, RowMajor> noisi_final_image(height, width);
-    noisi_final_image = noisy_image.unaryExpr([](double val) -> unsigned char {
-    return static_cast<unsigned char>(val * 255.0);
+  // Task 2 //
+  MatrixXd noisy_image = random_Noise_Generator(image);
+  Matrix<unsigned char, Dynamic, Dynamic, RowMajor> noisy_final_image(height, width);
+  noisy_final_image = noisy_image.unaryExpr([](double val) -> unsigned char {
+  return static_cast<unsigned char>(val * 255.0);
   });
+  const string output_noisy_image_path = "output_noisy.png";
+  if (stbi_write_png(output_noisy_image_path.c_str(), width, height, 1, noisy_final_image.data(), width) == 0) {
+    cerr << "Error: Could not save noisy image" << endl;
+    return 1;
+    }
+  cout << "Noisy image saved to " << output_noisy_image_path << endl;
 
+  // Task 3 //
+  VectorXd v=Map<VectorXd>(image.data(), image.size());
+  cout << "Reshape the image matrix into a vector v of size " << v.size() << ". Is the size equal to mn? " << ((v.size()==height*width)?"True":"False") << endl;
+  VectorXd w=Map<VectorXd>(noisy_image.data(), noisy_image.size());
+  cout << "Reshape the noisy image matrix into a vector w of size " << w.size() << ". Is the size equal to mn? " << ((w.size()==height*width)?"True":"False") << endl;
+  cout << "Norm of vector v, ||v||=" << v.norm() << endl;
 
-
-
-
-    return 0;
+  // Task 4 //
+  
+  return 0;
 }
