7703543b1a8249d96dfa8b8e36f9e558fd8728ea
giorgiovenezia <giorgio.venezia@mail.polimi.it>
2024-09-28 15:00:15 +0000

    task 5

--- project1.cpp
+++ project1.cpp
@@ -19,6 +19,7 @@ MatrixXd random_Noise_Generator(const MatrixXd& image) {
   return noise_Added_Image;
 }
 
+
 int main(int argc, char* argv[]){
   if (argc < 2) {
     cerr << "Usage: " << argv[0] << " <image_path>" << endl;
@@ -48,25 +49,44 @@ int main(int argc, char* argv[]){
   cout<<"The matrix has: "<<noisy_image.rows()<<" rows and "<<noisy_image.cols()<<" columns"<<endl;
 
   Matrix<unsigned char, Dynamic, Dynamic, RowMajor> noisy_final_image(height, width);
- 
- cout<<"The matrix has: "<<noisy_final_image.rows()<<" rows and "<<noisy_final_image.cols()<<" columns"<<endl;
+  
+  cout<<"The matrix has: "<<noisy_final_image.rows()<<" rows and "<<noisy_final_image.cols()<<" columns"<<endl;
 
   noisy_final_image = noisy_image.unaryExpr([](double val) -> unsigned char {
-  return static_cast<unsigned char>(val * 255.0);
+    return static_cast<unsigned char>(val * 255.0);
   });
+
   const string output_noisy_image_path = "output_noisy.png";
+
   if (stbi_write_png(output_noisy_image_path.c_str(), width, height, 1, noisy_final_image.data(), width) == 0) {
     cerr << "Error: Could not save noisy image" << endl;
     return 1;
-    }
+  }
+
   cout << "Noisy image saved to " << output_noisy_image_path << endl;
 
   // Task 3 //
-  VectorXd v=Map<VectorXd>(image.transpose().data(), image.size());
+  // VectorXd v=Map<VectorXd>(image.transpose().data(), image.size());
+  VectorXd v(height * width);
+  for(int i = 0; i < height; i++){
+    for(int j = 0; j< width; j++){
+      int index = i * width + j;
+      v(index) = image(i,j);
+    }
+  }
   cout << "Reshape the image matrix into a vector v of size " << v.size() << ". Is the size equal to mn? " << ((v.size()==height*width)?"True":"False") << endl;
 
-  VectorXd w=Map<VectorXd>(noisy_image.transpose().data(), noisy_image.size());
+  // VectorXd w=Map<VectorXd>(noisy_image.transpose().data(), noisy_image.size());
+  VectorXd w(height * width);
+  for(int i = 0; i < height; i++){
+    for(int j = 0; j< width; j++){
+      int index = i * width + j;
+      w(index) = noisy_image(i,j);
+    }
+  }
   cout << "Reshape the noisy image matrix into a vector w of size " << w.size() << ". Is the size equal to mn? " << ((w.size()==height*width)?"True":"False") << endl;
+
+
   cout << "Norm of vector v, ||v||=" << v.norm() << endl;
 
 
@@ -75,97 +95,89 @@ int main(int argc, char* argv[]){
   tripletList.reserve(9 * height * width); // Reserve space for triplets
   SparseMatrix<double, RowMajor> A1(height * width, height * width);
 
-  VectorXd v1(height * width);
-
-  for(int i = 0; i < height; i++){
-    for(int j = 0; j< width; j++){
-      int index = i * width + j;
-      v1(index) = image(i,j);
-    }
-  }
-
+  MatrixXd H_av2(3,3);
+  H_av2<<1, 1, 1,
+         1, 1, 1,
+         1, 1, 1;
+  H_av2/=9.0;
 
   for (int i = 0; i < height * width; i++) {
-
     //i=row*width+col
-      int row = i / width; // Current row
-      int col = i % width; // Current column
+    int row = i / width; // Current row
+    int col = i % width; // Current column
 
-      // if(i<10) cout<<v(i)<<"||"<<image(row, col)<<endl;
+     // if(i<10) cout<<v(i)<<"||"<<image(row, col)<<endl;
       
+    for (int j = -1; j <= 1; j++) {
+      for (int k = -1; k <= 1; k++) {
+        // Calculate neighbor indices
+        int neighborRow = row + j;
+        int neighborCol = col + k;
 
-      for (int j = -1; j <= 1; j++) {
-          for (int k = -1; k <= 1; k++) {
-              // Calculate neighbor indices
-              int neighborRow = row + j;
-              int neighborCol = col + k;
-
-              // Check if the neighbor indices are within bounds
-              if (neighborRow >= 0 && neighborRow < height && neighborCol >= 0 && neighborCol < width) {
-                  int neighborIndex = neighborRow * width + neighborCol; // Calculate the linear index
-                  tripletList.push_back(Triplet<double>(i, neighborIndex, 1.0 / 9.0));
-              }
+        // Check if the neighbor indices are within bounds
+        if (neighborRow >= 0 && neighborRow < height && neighborCol >= 0 && neighborCol < width) {
+          int neighborIndex = neighborRow * width + neighborCol; // Calculate the linear index
+          tripletList.push_back(Triplet<double>(i, neighborIndex, H_av2(j+1, k+1)));
           }
+        }
       }
   }
 
   A1.setFromTriplets(tripletList.begin(), tripletList.end());
-
-double sum = 0;
-for (int b = 0; b < 3; b++){
-for (int a = 0; a < height*width; a++){
-  if(A1.coeff(b,a) != 0){
-    cout << A1.coeff(b,a) << "in posizione: " << b << " " << a << endl;
-    sum += A1.coeff(b,a);
+  
+  double sum = 0;
+  for (int b = 0; b < 3; b++){
+    for (int a = 0; a < height*width; a++){
+      if(A1.coeff(b,a) != 0){
+        cout << A1.coeff(b,a) << "in posizione: " << b << " " << a << endl;
+        sum += A1.coeff(b,a);
+      }
+    }
+    cout << "Somma totale riga " << b << " di A1: " << sum << endl;
+    sum = 0;
   }
-}
-cout << "Somma totale riga " << b << " di A1: " << sum << endl;
-sum = 0;
-}
 
   cout << "Fino a qui tutto bene" << endl;
 
-  VectorXd smoothed_image=A1*v1;
-/*
+  VectorXd smoothed_vector=255*A1*v;
+
+
   cout << "Coefficiente di image: " << image(0,0) << endl;
   cout << "Coefficiente di v: " << v(0) << endl;
-  cout << "Coefficiente di smoothed_image: " << smoothed_image.coeff(0) << endl;
+  cout << "Coefficiente di smoothed_image: " << smoothed_vector.coeff(0) << endl;
 
   cout << "Coefficiente di image: " << image(1,0) << endl;
   cout << "Coefficiente di v: " << v(3250) << endl;
-  cout << "Coefficiente di smoothed_image: " << smoothed_image.coeff(3250) << endl;
+  cout << "Coefficiente di smoothed_image: " << smoothed_vector.coeff(3250) << endl;
 
-  cout << "number of elements smoothed_image" << smoothed_image.size() << ". Is the size equal to mn? " << ((smoothed_image.size()==height*width)?"True":"False") << endl;
-  cout << "Smoothed image rows: " << smoothed_image.rows() << " and cols: " << smoothed_image.cols() << endl;
+  cout << "number of elements smoothed_image" << smoothed_vector.size() << ". Is the size equal to mn? " << ((smoothed_vector.size()==height*width)?"True":"False") << endl;
+  cout << "Smoothed image rows: " << smoothed_vector.rows() << " and cols: " << smoothed_vector.cols() << endl;
   cout << "A1 rows: " << A1.rows() << " and cols: " << A1.cols() << endl;
 
-*/
   cout << "Fino a qui tutto bene 2" << endl;
 
-  smoothed_image *= 255.0;
+  MatrixXd smoothed_matrix(height, width);
 
-  MatrixXd smoothed(height, width);
-
-  cout<<"The matrix has: "<<smoothed.rows()<<" rows and "<<smoothed.cols()<<" columns"<<endl;
+  cout<<"The matrix has: "<<smoothed_matrix.rows()<<" rows and "<<smoothed_matrix.cols()<<" columns"<<endl;
   for (int i = 0; i < height; i++) {
     for (int j = 0; j < width; j++) {
-        int index = i * width + j;  // Row-major indexing
-        smoothed(i, j) = smoothed_image[index];
+      int index = i * width + j;  // Row-major indexing
+      smoothed_matrix(i, j) = smoothed_vector[index];
     }
-}
+  }
 
   cout << "Fino a qui tutto bene 3" << endl;
   cout << "Original image dimensions: " << image.rows() << "x" << image.cols() << endl;
   cout << "v vector size: " << v.size() << endl;
-  cout << "smoothed_image vector size: " << smoothed_image.size() << endl;
-  cout << "Final smoothed image dimensions: " << smoothed.rows() << "x" << smoothed.cols() << endl;
+  cout << "smoothed_image vector size: " << smoothed_vector.size() << endl;
+  cout << "Final smoothed image dimensions: " << smoothed_matrix.rows() << "x" << smoothed_matrix.cols() << endl;
+  
+  Matrix<unsigned char, Dynamic, Dynamic, RowMajor> smoothed_final_image = 
+  smoothed_matrix.unaryExpr([](double val) -> unsigned char {
+    return static_cast<unsigned char>(std::clamp(val, 0.0, 255.0));
+  });
 
-   Matrix<unsigned char, Dynamic, Dynamic, RowMajor> smoothed_final_image = 
-    smoothed.unaryExpr([](double val) -> unsigned char {
-        return static_cast<unsigned char>(std::clamp(val, 0.0, 255.0));
-    });
-
-    cout<<"The matrix has: "<<smoothed_final_image.rows()<<" rows and "<<smoothed.cols()<<" columns"<<endl;
+  cout<<"The matrix has: "<<smoothed_final_image.rows()<<" rows and "<<smoothed_final_image.cols()<<" columns"<<endl;
   const string output_smooth_image_path = "output_smooth.png";
   if (stbi_write_png(output_smooth_image_path.c_str(), width, height, 1, smoothed_final_image.data(), width) == 0) {
     cerr << "Error: Could not save noisy image" << endl;
@@ -173,6 +185,30 @@ sum = 0;
     }
   cout << "smooth image saved to " << output_smooth_image_path << endl;
 
+  // Task 5 //
+  VectorXd smoothed_noisy_vector=255*A1*w;
+  MatrixXd smoothed_noisy_matrix(height, width);
+
+  cout<<"The matrix has: "<<smoothed_noisy_matrix.rows()<<" rows and "<<smoothed_noisy_matrix.cols()<<" columns"<<endl;
+  for (int i = 0; i < height; i++) {
+    for (int j = 0; j < width; j++) {
+      int index = i * width + j;  // Row-major indexing
+      smoothed_noisy_matrix(i, j) = smoothed_noisy_vector[index];
+    }
+  }
+
+  Matrix<unsigned char, Dynamic, Dynamic, RowMajor> smoothed_noisy_final_image = 
+  smoothed_noisy_matrix.unaryExpr([](double val) -> unsigned char {
+    return static_cast<unsigned char>(std::clamp(val, 0.0, 255.0));
+  });
+
+  const string output_smooth_noisy_image_path = "output_smooth_noisy.png";
+  if (stbi_write_png(output_smooth_noisy_image_path.c_str(), width, height, 1, smoothed_noisy_final_image.data(), width) == 0) {
+    cerr << "Error: Could not save noisy image" << endl;
+    return 1;
+    }
+  cout << "smooth image saved to " << output_smooth_noisy_image_path << endl;
+  
 
   return 0;
 }
