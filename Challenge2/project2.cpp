4bef83f66722b9be1de0f56308a1558ac59440d7
giorgiovenezia <giorgio.venezia@mail.polimi.it>
2024-10-20 18:19:18 +0000

    Manca solo la 4

--- Challenge2/project2.cpp
+++ Challenge2/project2.cpp
@@ -163,12 +163,12 @@ int main(int argc, char* argv[]){
             }
         }
     }
-    
+
     //print results
-    cout << "C_40 has "<<C_40.rows()<<"x"<<C_40.cols()<<" and has non zero entries in C_40: " << non_zero_entries_C_40 << endl;
-    cout << "C_80 has "<<C_80.rows()<<"x"<<C_80.cols()<<" and has non zero entries in C_80: " << non_zero_entries_C_80 << endl;
-    cout << "D_40 has "<<D_40.rows()<<"x"<<D_40.cols()<<" and has non zero entries in D_40: " << non_zero_entries_D_40 << endl;
-    cout << "D_80 has "<<D_80.rows()<<"x"<<D_80.cols()<<" and has non zero entries in D_80: " << non_zero_entries_D_80 << endl;
+    cout << "Non zero entries in C_40: " << non_zero_entries_C_40 << endl;
+    cout << "Non zero entries in C_80: " << non_zero_entries_C_80 << endl;
+    cout << "Non zero entries in D_40: " << non_zero_entries_D_40 << endl;
+    cout << "Non zero entries in D_80: " << non_zero_entries_D_80 << endl;
 
 
     // Task 7 //
@@ -212,7 +212,7 @@ int main(int argc, char* argv[]){
         }
     }
 
-    cout << checkerboard.norm() << endl;
+    cout << "Norm of checkerboard matrix: " <<checkerboard.norm() << endl;
 
     // Task 9 //
 
@@ -228,15 +228,136 @@ int main(int argc, char* argv[]){
         return 1;
     }
 
-    cout << "checkerboard image saved to " << output_noisy_checkerboard_image_path << endl;
+    cout << "checkerboard noisy image saved to " << output_noisy_checkerboard_image_path << endl;
 
     // Task 10 // 
 
-    JacobiSVD<MatrixXd> svd3(noisy_checkerboard, ComputeThinU | ComputeThinV);
-    VectorXd singular_values_checkerboard = svd3.singularValues(); //The eigenvalues are already sorted in decreasing order
+    JacobiSVD<MatrixXd> svd_checkboard(noisy_checkerboard, ComputeFullU | ComputeFullV);
+    VectorXd singular_values_checkerboard = svd_checkboard.singularValues(); //The eigenvalues are already sorted in decreasing order
 
     for (int i = 0; i < 2; i++) {
         cout << "Singular value " << i << ": " << singular_values_checkerboard(i) << endl;
     }
+
+    // Task 11 // 
+
+    MatrixXd U_checkerboard = svd_checkboard.matrixU();
+    MatrixXd V_checkerboard = svd_checkboard.matrixV();
+    MatrixXd S_checkerboard = U_checkerboard.transpose() * noisy_checkerboard * V_checkerboard;
+
+
+    // for(int i = 0; i < singular_values2.size(); i++) {
+    //     cout << "Singular value " << i << ": " << singular_values2(i)<< " | Matrix value:" << S(i,i) << endl;
+    // }
+    cout << "Checkerboard rows: " << checkerboard.rows() << " | A columns: " << checkerboard.cols() << endl;
+    cout << "U rows: " << U_checkerboard.rows() << " | U columns: " << U_checkerboard.cols() << endl;
+    cout << "V rows: " << V_checkerboard.rows() << " | V columns: " << V_checkerboard.cols() << endl;
+    cout << "S rows: " << S_checkerboard.rows() << " | S columns: " << S_checkerboard.cols() << endl;
+    cout << "U is orthogonal? " << ((U_checkerboard.transpose()*U_checkerboard).isApprox(MatrixXd::Identity(U_checkerboard.cols(), U_checkerboard.cols()), 1.e-8) ? "True" : "False") << endl;
+    cout << "V is orthogonal? " << ((V_checkerboard.transpose()*V_checkerboard).isApprox(MatrixXd::Identity(V_checkerboard.cols(), V_checkerboard.cols()), 1.e-8) ? "True" : "False") << endl;
+    diagonal=true;
+    i=0;
+    j=0;
+    while(diagonal&&i<S_checkerboard.rows()&&j<S_checkerboard.cols()){
+        if(abs(S_checkerboard(i , j))>1.e-8&&i!=j) diagonal=false; // Print each element
+        i++;j++;
+    }
+    cout << "S is diagonal? " << (diagonal ? "True" : "False") << endl;
+    cout << "Norm of S: " << S_checkerboard.norm() << endl;
+
+    MatrixXd C_checkerboard_5 = U_checkerboard.block(0,0, U_checkerboard.rows(), 5);
+    MatrixXd C_checkerboard_10 = U_checkerboard.block(0,0, U_checkerboard.rows(), 10);
+    MatrixXd D_checkerboard_5(V_checkerboard.rows(), 5);
+    MatrixXd D_checkerboard_10(V_checkerboard.rows(), 10);
+
+    cout << "C_checkerboard_5 has " << C_checkerboard_5.rows() << "x"<<C_checkerboard_5.cols() << endl;
+    cout << "C_checkerboard_10 has " << C_checkerboard_10.rows() << "x"<<C_checkerboard_10.cols() << endl;
+    cout << "D_checkerboard_5 has " << D_checkerboard_5.rows() << "x"<<D_checkerboard_5.cols() << endl;
+    cout << "D_checkerboard_10 has " << D_checkerboard_10.rows() << "x"<<D_checkerboard_10.cols() << endl;
+    cout << "Number of singular values=" << singular_values_checkerboard.size() << endl;
+
+    for(int i=0; i<D_checkerboard_10.rows(); i++){
+        for(int j=0; j<D_checkerboard_10.cols(); j++){
+            // cout<<"i="<<i<<", j="<<j<<endl;
+            // cout<<V(i,j)<<endl;
+            // cout<<D_80(i,j)<<endl;
+            // cout<<singular_values2(j)<<endl;
+            if(j<D_checkerboard_5.cols()){
+                // cout<<D_40(i,j)<<endl;
+                D_checkerboard_5(i, j)=V_checkerboard(i,j)* singular_values_checkerboard(j);
+            }
+            D_checkerboard_10(i, j)=V_checkerboard(i, j)*singular_values_checkerboard(j);
+        }
+    }
+
+    //compute non zero entries taking care of the machine precision
+    int non_zero_entries_C_checkerboard_5 = 0;
+    int non_zero_entries_C_checkerboard_10 = 0;
+    int non_zero_entries_D_checkerboard_5 = 0;
+    int non_zero_entries_D_checkerboard_10 = 0;
+    for(int i = 0; i < C_checkerboard_5.rows(); i++) {
+        for(int j = 0; j < C_checkerboard_5.cols(); j++) {
+            if(C_checkerboard_5(i,j) > 1.e-9) {
+                non_zero_entries_C_checkerboard_5++;
+            }
+        }
+    }
+    for(int i = 0; i < C_checkerboard_10.rows(); i++) {
+        for(int j = 0; j < C_checkerboard_10.cols(); j++) {
+            if(C_checkerboard_10(i,j) > 1.e-9) {
+                non_zero_entries_C_checkerboard_10++;
+            }
+        }
+    }
+    for(int i = 0; i < D_checkerboard_5.rows(); i++) {
+        for(int j = 0; j < D_checkerboard_5.cols(); j++) {
+            if(D_checkerboard_5(i,j) > 1.e-9) {
+                non_zero_entries_D_checkerboard_5++;
+            }
+        }
+    }
+    for(int i = 0; i < D_checkerboard_10.rows(); i++) {
+        for(int j = 0; j < D_checkerboard_10.cols(); j++) {
+            if(D_checkerboard_10(i,j) > 1.e-9) {
+                non_zero_entries_D_checkerboard_10++;
+            }
+        }
+    }
+
+    //print results
+    cout << "Non zero entries in C_checkerboard_5: " << non_zero_entries_C_checkerboard_5 << endl;
+    cout << "Non zero entries in C_checkerboard_10: " << non_zero_entries_C_checkerboard_10 << endl;
+    cout << "Non zero entries in D_checkerboard_5: " << non_zero_entries_D_checkerboard_5 << endl;
+    cout << "Non zero entries in D_checkerboard_10: " << non_zero_entries_D_checkerboard_10 << endl;
+
+    // Task 12 // 
+
+    MatrixXd checkerboard_tilde_5=C_checkerboard_5*D_checkerboard_5.transpose();
+    MatrixXd checkerboard_tilde_10=C_checkerboard_10*D_checkerboard_10.transpose();
+
+    Matrix<unsigned char, Dynamic, Dynamic, RowMajor> checkerboard_tilde_5_image = checkerboard_tilde_5.unaryExpr([](double val) -> unsigned char {
+         return static_cast<unsigned char>(std::clamp(val, 0.0, 255.0));
+    });
+
+    const string output_checkerboard_tilde_5_image_path = "checkerboard_tilde_5_image.png";
+    if (stbi_write_png(output_checkerboard_tilde_5_image_path.c_str(), checkerboard_tilde_5_image.cols(), checkerboard_tilde_5_image.rows(), 1, checkerboard_tilde_5_image.data(), checkerboard_tilde_5_image.cols()) == 0) {
+        cerr << "Error: Could not save checkerboard_tilde_5 image" << endl;
+        return 1;
+    }
+
+    cout << "checkerboard_tilde_5 image saved to " << output_checkerboard_tilde_5_image_path << endl;
+
+     Matrix<unsigned char, Dynamic, Dynamic, RowMajor> checkerboard_tilde_10_image = checkerboard_tilde_10.unaryExpr([](double val) -> unsigned char {
+         return static_cast<unsigned char>(std::clamp(val, 0.0, 255.0));
+    });
+
+    const string output_checkerboard_tilde_10_image_path = "checkerboard_tilde_10_image.png";
+    if (stbi_write_png(output_checkerboard_tilde_10_image_path.c_str(), checkerboard_tilde_10_image.cols(), checkerboard_tilde_10_image.rows(), 1, checkerboard_tilde_10_image.data(), checkerboard_tilde_10_image.cols()) == 0) {
+        cerr << "Error: Could not save checkerboard_tilde_10 image" << endl;
+        return 1;
+    }
+
+    cout << "checkerboard_tilde_10 image saved to " << output_checkerboard_tilde_10_image_path << endl;   
+
     return 0;
 }
